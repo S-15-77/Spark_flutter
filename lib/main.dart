9a4450ad79be5b1650319de62690bd92ba7856ec
S-15-77 <santhoshdayakar2002@gmail.com>
2021-06-05 18:23:46 +0530

    spark

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:social_app/profile/profile_2.dart';
 import 'screens/welcome_page.dart';
 import 'profile/profile_1.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: AnimatedSplashScreen(
+        splash: 'images/spark.png',
+        backgroundColor: Colors.tealAccent,
+        nextScreen: Sec(),
+        splashTransition: SplashTransition.fadeTransition,
+        duration: 3000,
+      ),
+    );
+  }
+}
+
+class Sec extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
